89080222e0d93f9e0c9c45897c696fe208d6d6c7
IvanTitskiy <titskiyivan@gmail.com>
2019-10-11 20:45:35 +0300

    Update details_items.php

--- details_items.php
+++ details_items.php
@@ -1,16 +1,1 @@
-<?php  // add 11.10.2019
-    $productsInCart = array();
-    $productsInCart2 = array();
-    $productsInCartOld = $this->orderdetails['items'];
-    foreach ($productsInCartOld as $key => $row) {
-        $productsInCart[$key] = $row->category_name;
-        $productsInCart2[$key] = $row->product_name;
-    }
-    //array_multisort($productsInCart, SORT_ASC, $productsInCartOld);
-    array_multisort($productsInCart, SORT_ASC, $productsInCart2, SORT_ASC, $productsInCartOld);
-	//foreach($this->orderdetails['items'] as $item) {
-    foreach($productsInCartOld as $item) {
-    //END: add 11.10.201900
-		$qtt = $item->product_quantity ;
-		$_link = JRoute::_('index.php?option=com_virtuemart&view=productdetails&virtuemart_category_id=' . $item->virtuemart_category_id . '&virtuemart_product_id=' . $item->virtuemart_product_id, FALSE);
-		?>
+cccc
